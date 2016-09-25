4653d218c9c13445760b693db2e6d697a58e40fc
Jon McKay <johnnyman727@gmail.com>
2016-09-24 23:59:52 -0700

    Get deep net working

--- deep_net.py
+++ deep_net.py
@@ -27,18 +27,18 @@ tvX, tvY = data.validate.features, data.validate.labels;
 teX, teY = data.test.features, data.test.labels;
 
 X = tf.placeholder("float32", [None, 5])
-Y = tf.placeholder("float32", [None, 2])
+Y = tf.placeholder("float32", [None, 1])
 
 w_h = init_weights([5, 100])
 w_h2 = init_weights([100, 100])
-w_o = init_weights([100, 2])
+w_o = init_weights([100, 1])
 
 p_keep_input = tf.placeholder("float")
 p_keep_hidden = tf.placeholder("float")
 py_x = model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden)
 #
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(py_x, Y))
+train_op = tf.train.RMSPropOptimizer(0.0001, 0.8).minimize(cost)
 predict_op = tf.nn.sigmoid(py_x)
 
 # Launch the graph in a session
@@ -47,11 +47,19 @@ with tf.Session() as sess:
     tf.initialize_all_variables().run()
 
     for i in range(100):
-        for start, end in zip(range(0, len(trX), 10), range(10, len(trX)+1, 10)):
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
             sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
                                           p_keep_input: 0.8, p_keep_hidden: 0.5})
 
-        print(i, np.mean(np.argmax(teY, axis=1) ==
-                         sess.run(predict_op, feed_dict={X: teX, Y: teY,
-                                                         p_keep_input: 1.0,
-                                                         p_keep_hidden: 1.0})))
+        print(i, 'cost:', sess.run(cost, feed_dict={X: tvX, Y:tvY, p_keep_input: 1.0, p_keep_hidden: 1.0}));
+        print(i, 'accuracy:', (np.round(sess.run(predict_op, feed_dict={X: tvX,
+                                p_keep_input: 1.0, p_keep_hidden: 1.0})) == tvY).mean())
+
+
+        # print(i, np.mean(np.argmax(teY, axis=1) ==
+                        #  sess.run(predict_op, feed_dict={X: teX, Y: teY,
+                                                        #  p_keep_input: 1.0,
+                                                        #  p_keep_hidden: 1.0})))
+    print('Final score:', (np.round(sess.run(predict_op, feed_dict={X: teX,
+                                        p_keep_input: 1.0,
+                                        p_keep_hidden: 1.0})) == teY).mean())
