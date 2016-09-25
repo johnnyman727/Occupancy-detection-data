47289aab98cc535362abb54b91b93fa57dddacf8
Jon McKay <johnnyman727@gmail.com>
2016-09-24 23:47:02 -0700

    fixes predictions with simple net

--- simple_net.py
+++ simple_net.py
@@ -5,8 +5,8 @@ import numpy as np
 import input_data
 import matplotlib.pyplot as plt
 
-def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+def init_weights(shape, dev):
+    return tf.Variable(tf.random_normal(shape, stddev=dev))
 
 
 def model(X, w_h, w_o):
@@ -21,14 +21,14 @@ teX, teY = data.test.features, data.test.labels;
 X = tf.placeholder("float32", [None, 5])
 Y = tf.placeholder("float32", [None, 1])
 
-w_h = init_weights([5, 100]) # create symbolic variables
-w_o = init_weights([100, 1])
+w_h = init_weights([5, 5], np.sqrt(2 / np.prod(X.get_shape().as_list()[1:]))) # create symbolic variables
+w_o = init_weights([5, 1], np.sqrt(2 / np.prod(Y.get_shape().as_list()[1:])))
 
 py_x = model(X, w_h, w_o)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y)) # compute costs
+cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(py_x, Y)) # compute costs
 train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct an optimizer
-predict_op = tf.argmax(py_x, 1)
+predict_op = tf.nn.sigmoid(py_x);
 
 cost_history = [];
 accuracy_history = [];
@@ -39,18 +39,16 @@ with tf.Session() as sess:
     tf.initialize_all_variables().run()
 
     for i in range(300):
-        for start, end in zip(range(0, len(trX), 128), range(10, len(trX)+1, 128)):
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
             sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]});
 
         cost_history.append(sess.run(cost, feed_dict={X: tvX, Y:tvY}));
         print(i, 'cost', cost_history[len(cost_history) - 1]);
-        accuracy_history.append(np.mean(np.argmax(tvY, axis=1) ==
-                         sess.run(predict_op, feed_dict={X: tvX})))
+        accuracy_history.append((np.round(sess.run(predict_op, feed_dict={X: tvX})) == tvY).mean())
         print(i, 'acc', accuracy_history[len(accuracy_history)-1])
 
 
-    print('Final score:', np.mean(np.argmax(teY, axis=1) ==
-                     sess.run(predict_op, feed_dict={X: teX})))
+    print('Final score:', (np.round(sess.run(predict_op, feed_dict={X: teX})) == teY).mean())
 
 plt.plot(cost_history, 'r--');
 plt.text(120, .35, r'Cost')
